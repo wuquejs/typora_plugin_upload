8d2b19185e63649c5d9f4ecf9c020e572d424c71
wuque <wuquejs@163.com>
2022-02-07 21:27:43 +0800

    修复上传网络图片时，无法上传,且typora会自动加上一串乱码的问题

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 **本插件是基于markdown编辑器Typora软件写的一个自动上传小插件**
 
 
+# 更新日志
+- 2022-02-07 1.0.3
+  - 修复上传网络图片时，无法上传,且typora会自动加上一串乱码的问题
+
 
 # 功能
 
@@ -27,7 +31,7 @@
 - 命令填入如下代码，以我打包好的jar包为例
 
   ```
-  java -cp jar所在的绝对路径.jar cc.wuque.typora_plugin_upload.Main
+  java -jar jar所在的绝对路径.jar
   ```
 
 ![image-20211107163412621](http://ftp.25wz.cn/study/image-20211107163412621.png)
@@ -45,7 +49,7 @@
 - 命令填入如下代码，以我打包好的jar包为例
 
   ```
-  java -cp jar所在的绝对路径.xx.jar  cc.wuque.typora_plugin_upload.Main
+      java -jar jar所在的绝对路径.jar
   ```
 
 - 在编辑markdown时直接粘贴图片然后右键上传图片即可
--- src/main/java/cc/wuque/typora_plugin_upload/Main.java
+++ src/main/java/cc/wuque/typora_plugin_upload/Main.java
@@ -1,6 +1,9 @@
 package cc.wuque.typora_plugin_upload;
 
 import cc.wuque.typora_plugin_upload.upload.FtpUpload;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.http.HttpRequest;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,23 +18,40 @@ import java.io.InputStream;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        if (!checkArgs(args)) {
+        if (args.length == 0) {
             return;
         }
+        InputStream inputStream;
+        String fileName = RandomUtil.randomString(10) + ".jpg";
         FtpUpload ftpUpload = new FtpUpload();
-
-        for (String arg : args) {
-            File file = new File(arg);
-            try {
-                InputStream inputStream = new FileInputStream(file);
-                String upload = ftpUpload.upload(file.getName(), inputStream);
-                System.out.println(upload);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        for (String filePath : args) {
+            if (!new File(filePath).exists()) {
+                inputStream = HttpRequest.get(filePath).execute().bodyStream();
+            }else {
+                File file = new File(filePath);
+                inputStream = new FileInputStream(file);
+                fileName = file.getName();
             }
+
+            String upload = ftpUpload.upload(fileName, inputStream);
+            System.out.println(upload);
+
         }
+
+
+
+//        for (String arg : args) {
+//            File file = new File(arg);
+//            try {
+//                InputStream inputStream = new FileInputStream(file);
+//                String upload = ftpUpload.upload(file.getName(), inputStream);
+//                System.out.println(upload);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     /**
@@ -43,13 +63,13 @@ public class Main {
      */
     public static Boolean checkArgs(String[] args) {
         boolean checkResult = true;
+        //如果没有传入参数则直接推出程序
         if (args.length == 0) {
-            System.out.println("输入参数不能为空");
             checkResult = false;
         }
         for (String filePath : args) {
             if (!new File(filePath).exists()) {
-                System.out.println(filePath + " 文件不存在");
+                //System.out.println(filePath + " 文件不存在");
                 checkResult = false;
             }
         }
